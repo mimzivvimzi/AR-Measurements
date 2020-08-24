51b60e2853d6837e2ac86a335d5d21842967664d
Michelle Lau <mdeniselau@gmail.com>
2020-08-24 23:02:26 +0900

    Remove lin lineGeometry

--- ARMeasurements/ViewController.swift
+++ ARMeasurements/ViewController.swift
@@ -76,16 +76,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         )
         sceneView.scene.rootNode.addChildNode(pointNode)
         pointNodes.append(pointNode)
-        vertices.append(pointNode.position)
-        let linesGeometry = SCNGeometry(sources: [SCNGeometrySource(vertices: vertices)],
-        elements: [SCNGeometryElement(indices: [Int32]([0, 1]), primitiveType: .line)])
-        let line = SCNNode(geometry: linesGeometry)
-        if vertices.count == 2 {
-            sceneView.scene.rootNode.addChildNode(line)
-            vertices = [SCNVector3]()
-        } else if vertices.count > 2 {
-            line.removeFromParentNode()
-        }
+//        vertices.append(pointNode.position)
+//        let linesGeometry = SCNGeometry(sources: [SCNGeometrySource(vertices: vertices)],
+//        elements: [SCNGeometryElement(indices: [Int32]([0, 1]), primitiveType: .line)])
+//        let line = SCNNode(geometry: linesGeometry)
+//        if vertices.count == 2 {
+//            sceneView.scene.rootNode.addChildNode(line)
+//            vertices = [SCNVector3]()
+//        } else if vertices.count > 2 {
+//            line.removeFromParentNode()
+//        }
         if pointNodes.count >= 2 {
             measure()
         }
